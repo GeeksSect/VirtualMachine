08112d1d1441e350638884bad490b89381a11274
alexFrankfurt <alex_frankfurt@mail.ru>
2015-04-20 09:22:02 +0300

    [+] add reading program from file fixes #15

--- source/gsvm/main.d
+++ source/gsvm/main.d
@@ -2,6 +2,14 @@
 
 import std.stdio;
 import std.conv;
+import spec.common;
+import std.json;
+import std.stdio;
+import std.range;
+import std.traits;
+import std.algorithm;
+import std.array;
+import std.typecons;
 
 import gsvm.vm;
 import gsvm.models.storage;
@@ -10,24 +18,28 @@ void main(string[] args)
 {
 	try
 	{
+		File programFile = File(args[1],"r");
 		auto memory = Storage(0xF000, TypeOfStorage.RAM);
-		memory.write(0x00, 0x4730_4311u); // and
-		memory.write(0x04, 0x0000_FFFFu); // arg 1
-		memory.write(0x08, 0xFFFF_000Fu); // arg 2 
-		memory.write(0x0C, 0x0000_0000u); // arg 3
-		memory.write(0x10, 0x4730_4313u); // xor
-		memory.write(0x14, 0x0000_FFFFu); // arg 1
-		memory.write(0x18, 0xFFFF_0000u); // arg 2 
-		memory.write(0x1C, 0x0000_0004u); // arg 3
-		memory.write(0x20, 0x4710_0131u); // jmp
-		memory.write(0x24, 0x0000_0000u); // arg 1
+
+		programFile.writeBytesIn(memory);
+//		auto memory = Storage(0xF000, TypeOfStorage.RAM);
+//		memory.write(0x00, 0x4730_4311u); // and
+//		memory.write(0x04, 0x0000_FFFFu); // arg 1
+//		memory.write(0x08, 0xFFFF_000Fu); // arg 2 
+//		memory.write(0x0C, 0x0000_0000u); // arg 3
+//		memory.write(0x10, 0x4730_4313u); // xor
+//		memory.write(0x14, 0x0000_FFFFu); // arg 1
+//		memory.write(0x18, 0xFFFF_0000u); // arg 2 
+//		memory.write(0x1C, 0x0000_0004u); // arg 3
+//		memory.write(0x20, 0x4710_0131u); // jmp
+//		memory.write(0x24, 0x0000_0000u); // arg 1
 		memory.dump(0,0x28);
-		auto vm = ProcessorCore(&memory);
-		vm.loadProgramm(0x00);
-		vm.runOneCommand();
-		vm.runOneCommand();
-		vm.runOneCommand();
-		vm.runOneCommand();
+//		auto vm = ProcessorCore(&memory);
+//		vm.loadProgramm(0x00);
+//		vm.runOneCommand();
+//		vm.runOneCommand();
+//		vm.runOneCommand();
+//		vm.runOneCommand();
 	}
 	catch(Exception e)
 	{
@@ -36,3 +48,13 @@ void main(string[] args)
 	stdin.readln();
 }
 
+void writeBytesIn(File sourceFile, Storage memory)
+{
+	auto byteFlow = sourceFile.byChunk(4096).joiner.array;
+
+	uint curPos = 0x00;
+	foreach(bt; byteFlow) {
+		memory.write(curPos, bt);
+		curPos += 1;
+	}
+}
