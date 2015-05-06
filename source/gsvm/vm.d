b8bc16c72c90607208fc06e2f38a2f5925487f79
Stankievič Alaksiej <HaraldZealot@users.noreply.github.com>
2015-05-06 23:47:52 +0300

    [+] turtle step 2

--- source/gsvm/vm.d
+++ source/gsvm/vm.d
@@ -406,7 +406,7 @@ void handler(ubyte opcode)(ref ProcessorCore pc)
 void handler(ubyte opcode)(ref ProcessorCore pc)
 	if(opcode == OperationCode.SHR)
 {
-	//TODO logical right shift handler
+	pc.calcRegisters[2] = pc.calcRegisters[0] >>> pc.calcRegisters[1];
 }
 
 void handler(ubyte opcode)(ref ProcessorCore pc)
