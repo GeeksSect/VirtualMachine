7047158971b12751c64a68cf5413973ab52a0df2
Harald Zealot <harald_zealot@tut.by>
2015-05-18 21:36:52 +0300

    [+] turtle step 9

--- source/gsvm/vm.d
+++ source/gsvm/vm.d
@@ -460,7 +460,7 @@ void handler(ubyte opcode)(ref ProcessorCore pc)
 void handler(ubyte opcode)(ref ProcessorCore pc)
 	if(opcode == OperationCode.ADD)
 {
-	//TODO addition handler
+	pc.calcRegisters[2] = pc.calcRegisters[0] + pc.calcRegisters[1];
 }
 
 void handler(ubyte opcode)(ref ProcessorCore pc)
